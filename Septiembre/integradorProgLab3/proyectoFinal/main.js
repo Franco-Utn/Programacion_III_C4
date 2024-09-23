5a1ca7db89d7d28b3cd18062c5ad33ea813fc617
FrancoUtn <francosardiutn@gmail.com>
2024-09-22 23:09:41 -0300

    Proyecto Final Integrador

--- Septiembre/integradorProgLab3/proyectoFinal/main.js
+++ Septiembre/integradorProgLab3/proyectoFinal/main.js
@@ -1,60 +1,31 @@
-import { setInLocalStorage } from "../src/persistence/localStorage";
-import { renderCategories } from "../src/services/categories";
-import { handleGetProductsToStorage } from "../src/views/store";
-import './style.css'
+import { openModal } from "./src/views/modal";
+import { renderCategories } from "./src/services/categories";
+import { handleGetProductsToStore } from "./src/views/store";
+import { handleSearchProductByName } from "./src/services/searchBar";
+import "./style.css";
 
-handleGetProductsToStorage()
-renderCategories()
-//Product
+//===========APLICACION============
+export let categoriaActiva = null;
+export const setCategoriaActiva = (categoriaIn)=>{
+    categoriaActiva = categoriaIn;
+};
 
-const buttonAdd = document.getElementById("buttonAddElement")
+export let productoActivo = null;
+export const setProductoActivo = (productoIn)=>{
+    productoActivo = productoIn;
+};
 
-buttonAdd.addEventListener("click",()=>{
-    openModal()
-})
+handleGetProductsToStore();
+renderCategories();
 
-// PopUP
+//HEADER
+const buttonAdd = document.getElementById("buttonAddElement");
+buttonAdd.addEventListener('click', ()=>{
+    openModal();
+});
 
-const buttonCancel = document.getElementById("cancelButton")
-
-buttonCancel.addEventListener("click",()=>{
-    handleCancelButton()
-})
-
-const handleCancelButton=()=>{
-    closeModal();
-}
-
-//Funciones Abrir Cerrar Modal
-const openModal = ()=>{
-    const modal = document.getElementById("modalPopUp")
-    modal.style.display = "flex"
-}
-const closeModal = ()=>{
-    const modal = document.getElementById("modalPopUp"); 
-    modal.style.display = "none"; // Esto cerrará el modal
-}
-
-//Guardar o Modificar elemntos
-const acceptButton = document.getElementById("acceptButton")
-acceptButton.addEventListener("click",()=>{
-    handleSaveModifyElements()
-})
-const handleSaveModifyElements =()=>{
-    const nombre = document.getElementById("name").value,
-    imagen = document.getElementById("img").value,
-    precio = document.getElementById("precio").value,
-    categoria = document.getElementById("categoria").value
-   
-    let object={
-        id: new Date().toISOString(),
-        nombre,
-        imagen,
-        precio,
-        categoria
-    }
-
-    console.log("Producto a guardar:", object); // Verifica que los datos no estén vacíos
-    setInLocalStorage(object);
-    closeModal();
-}
+//buttonSearch
+const buttonSearch = document.getElementById("buttonSearch");
+buttonSearch.addEventListener('click', ()=>{
+    handleSearchProductByName();
+});
